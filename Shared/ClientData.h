698d0e3d39458f28934ca318494d13a3f842d9d0
ChunanGang <chh179@ucsd.edu>
2019-06-08 05:01:04 -0700

    0.8

--- Shared/ClientData.h
+++ Shared/ClientData.h
@@ -7,8 +7,11 @@ struct ClientData
 	
 	glm::vec3 head_pos;
 	glm::quat head_rot;
+	glm::vec3 player_translation;
+	float player_rotation;
 	glm::vec3 controller_location[2];
 	glm::quat controller_rotation[2];
+	glm::vec4 cue_point;
 	bool hit;
 
 	// rpc Macro to generate serialize code for the struct (Note: for glm object, manually specify x,y,z,w)
@@ -16,10 +19,14 @@ struct ClientData
 	(
 		head_pos.x, head_pos.y, head_pos.z, 
 		head_rot.x, head_rot.y, head_rot.z, head_rot.w,
+		player_translation.x, player_translation.y, player_translation.z,
+		player_rotation,
 		controller_location[0].x, controller_location[0].y, controller_location[0].z,
 		controller_location[1].x, controller_location[1].y, controller_location[1].z,
 		controller_rotation[0].x, controller_rotation[0].y, controller_rotation[0].z, controller_rotation[0].w,
 		controller_rotation[1].x, controller_rotation[1].y, controller_rotation[1].z, controller_rotation[1].w,
+		// cue
+		cue_point.x, cue_point.y, cue_point.z, cue_point.w,
 		hit
 	)
 };
